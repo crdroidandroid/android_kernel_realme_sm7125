ebc18aab6ee5d04d1fb226d7fa9486c2d8ed42e7
Deepak Kumar <dkumar@codeaurora.org>
2019-06-26 10:37:22 +0530

    msm: kgsl: Use event workqueue for event work instead of RT Kthread worker
    Currently, both dispatcher work and event work is handled by real time
    kthread worker. This results in two problems:
    
    1) Event work can result in freeing memory either as part of
    free on timestamp or while putting a context. As freeing memory
    can take time it shouldn't be done as part of RT thread as it can
    result in hogging CPU for longer duration and not giving chance to
    other tasks to run.
    
    2) Event work remains unprocessed till kthread worker finishes
    dispatcher work. As dispatcher work first schedules the event
    work for previously retired commands and then tries to submit
    new commands. The scheduled event work doesn't get processed till
    dispatcher work finishes. This can result in even longer delays
    in case dispatcher work gets blocked after scheduling event work
    because of mutexes locks etc.
    
    To avoid this, use event workqueue for event work. This will decouple
    both dispatcher and event work and will also make sure that high
    latency work like freeing-up memory is not done as part of RT thread.
    
    Change-Id: I74734a1bc177feecaac6626109349752da44da80
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>
    
     Conflicts:
    	drivers/gpu/msm/kgsl.h
    	drivers/gpu/msm/kgsl_events.c

--- drivers/gpu/msm/kgsl_events.c
+++ drivers/gpu/msm/kgsl_events.c
@@ -32,23 +32,7 @@ static inline void signal_event(struct kgsl_device *device,
 {
 	list_del(&event->node);
 	event->result = result;
-	if (event->prio == KGSL_EVENT_LOW_PRIORITY)
-		kthread_queue_work(&kgsl_driver.low_prio_worker, &event->work);
-	else
-		kthread_queue_work(&kgsl_driver.worker, &event->work);
-}
-
-static const char *priorities[KGSL_EVENT_NUM_PRIORITIES] = {
-	"KGSL_EVENT_REGULAR_PRIORITY",
-	"KGSL_EVENT_LOW_PRIORITY"
-};
-
-const char *prio_to_string(enum kgsl_priority prio)
-{
-	if (prio < KGSL_EVENT_NUM_PRIORITIES)
-		return priorities[prio];
-	else
-		return "<invalid priority>";
+	queue_work(device->events_wq, &event->work);
 }
 
 /**
@@ -298,10 +282,6 @@ static int kgsl_add_event_common(struct kgsl_device *device,
 	event->prio = prio;
 
 	INIT_WORK(&event->work, _kgsl_event_worker);
-#ifdef OPLUS_FEATURE_UIFIRST
-// caichen@TECH.Kernel.Sched, 2020/05/28, add for uifirst wq
-	set_uxwork(&event->work);
-#endif
 
 	trace_kgsl_register_event(
 		KGSL_CONTEXT_ID(context), timestamp, func, prio);
@@ -317,11 +297,8 @@ static int kgsl_add_event_common(struct kgsl_device *device,
 
 	if (timestamp_cmp(retired, timestamp) >= 0) {
 		event->result = KGSL_EVENT_RETIRED;
-		if (prio == KGSL_EVENT_LOW_PRIORITY)
-			kthread_queue_work(
-				&kgsl_driver.low_prio_worker, &event->work);
-		else
-			kthread_queue_work(&kgsl_driver.worker, &event->work);
+
+		queue_work(device->events_wq, &event->work);
 		spin_unlock(&group->lock);
 		return 0;
 	}
