8e8d456aebfbdf13ef68f82f363944ee58415b56
Zhongqiu Han <quic_zhonhan@quicinc.com>
2024-10-09 17:37:45 +0800

    sched: idle: Optimize the generic idle loop by removing needless memory barrier
    The memory barrier rmb() in generic idle loop do_idle() function is not
    needed, it doesn't order any load instruction, just remove it as needless
    rmb() can cause performance impact.
    
    The rmb() was introduced by the tglx/history.git commit f2f1b44c75c4
    ("[PATCH] Remove RCU abuse in cpu_idle()") to order the loads between
    cpu_idle_map and pm_idle. It pairs with wmb() in function cpu_idle_wait().
    
    And then with the removal of cpu_idle_state in function cpu_idle() and
    wmb() in function cpu_idle_wait() in commit 783e391b7b5b ("x86: Simplify
    cpu_idle_wait"), rmb() no longer has a reason to exist.
    
    After that, commit d16699123434 ("idle: Implement generic idle function")
    implemented a generic idle function cpu_idle_loop() which resembles the
    functionality found in arch/. And it retained the rmb() in generic idle
    loop in file kernel/cpu/idle.c.
    
    And at last, commit cf37b6b48428 ("sched/idle: Move cpu/idle.c to
    sched/idle.c") moved cpu/idle.c to sched/idle.c. And commit c1de45ca831a
    ("sched/idle: Add support for tasks that inject idle") renamed function
    cpu_idle_loop() to do_idle().
    
    History Tree: https://git.kernel.org/pub/scm/linux/kernel/git/tglx/history.git
    Signed-off-by: Zhongqiu Han <quic_zhonhan@quicinc.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20241009093745.9504-1-quic_zhonhan@quicinc.com
    Change-Id: I7d04d05f25b66ab266b66424dfddd58857e5242b
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -245,6 +245,8 @@ static void do_idle(void)
 	while (!need_resched()) {
 		check_pgt_cache();
 
+		local_irq_disable();
+
 		if (cpu_is_offline(smp_processor_id())) {
 			tick_nohz_idle_stop_tick_protected();
 			cpuhp_report_idle_dead();
