3d06051a87550ff4a41477bd8e43900c91806b42
theshaenix <theshaenix@gmail.com>
2026-01-07 21:53:01 +0530

    mm: Set swappiness to 85

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -177,7 +177,7 @@ struct scan_control {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 85;
 #if defined(OPLUS_FEATURE_ZRAM_OPT) && defined(CONFIG_OPLUS_ZRAM_OPT)
 /*
  * Direct reclaim swappiness, exptct 0 - 60. Higher means more swappy and slower.
